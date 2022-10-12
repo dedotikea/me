8ed72b0aea07397dd27f8089a81dfe66b6ebcb27
rayhan <rayhangustiar@gmail.com>
2022-10-12 10:20:04 +0700

    Nambahin bgchange, header, sama favicon

---
+++ src/HeaderText.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HeaderText = () => {
+    return (
+        <div className="header-text">
+            <h1>
+                dedotikea-me
+            </h1>
+        </div>
+    )
+}
+
+export default HeaderText;
