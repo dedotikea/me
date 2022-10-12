746e60fc5732c62f0681b5d3c327c4a5b4823801
rayhan <rayhangustiar@gmail.com>
2022-10-12 19:21:35 +0700

    update what to do

--- src/App.js
+++ src/App.js
@@ -23,8 +23,13 @@ function App() {
 export default App;
 
 //what to do:
-//nambahin tip (click to copy) pas cursor ngehover discord
-//nambahin tip (copied) pas cursor ngeclick discord
-//animasi berhenti pas cursor ngehover ke salah satu link
-//ngebenerin tampilan di mobile
-//kapan harus pake synchronous/asynchronous(?)
+
+//1. nambahin tip (click to copy) pas cursor ngehover discord
+//2. nambahin tip (copied) pas cursor ngeclick discord
+//3. animasi berhenti pas cursor ngehover ke salah satu link
+//4. ngebenerin tampilan di mobile
+//5. kapan harus pake synchronous/asynchronous(?)
+//6. bikin bgchanges lebih singkat/simpel(?)
+//7. routing ke page lain(?)
+//8. bikin footer buat nambahin source art (yoneyama)
+//9. ganti dari img ke background
